bab0f7ab01802161607b075d56f32f1033c7fd50
Oleh Klym <oleh.klym@teladochealth.com>
2023-08-24 13:52:51 +0300

    Added seeds for creating User

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+50.times do |n|
+  User.create(
+    first_nm: Faker::Name.first_name,
+    last_nm: Faker::Name.last_name,
+    age: Faker::Number.number(digits: 2),
+    gender: Faker::Gender.binary_type
+  )
+end
