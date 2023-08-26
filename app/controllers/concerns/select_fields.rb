0327a727f02efcc5c4509fc550c757240b4a11e5
Oleh Klym <oleh.klym@teladochealth.com>
2023-08-26 16:03:52 +0300

    Fixed bug related to no fields param passed

--- app/controllers/concerns/select_fields.rb
+++ app/controllers/concerns/select_fields.rb
@@ -1,5 +1,6 @@
 module SelectFields
   extend ActiveSupport::Concern
+  ALL_FIELDS = ['*']
 
   class_methods do
     def with_selected_fields(fields)
@@ -7,6 +8,8 @@ module SelectFields
     end
 
     def validated_fields(fields)
+      return ALL_FIELDS if fields == :all
+
       fields.keep_if { |field| field.in?(self.attribute_names) }
     end
   end
